64d08b49b5e8113cef46338877f563a05eee0521
Al Bluemle <abluemleiv@gmail.com>
2021-03-24 13:40:16 -0400

    Adds functionality if the directory fails it can be rerun.
    Sets the new "run" variable to True. Puts the input and function call into a while loop, which runs while True. The check_files function will now return True if the file directory is invalid - meaning the loop will start again and ask for new inputs. The rename_files function will return False when completed - meaning the while loop will exit and the program will end.

--- sequence_files.py
+++ sequence_files.py
@@ -2,15 +2,14 @@ import os
 import re
 
 
-def rename_files(location, name, extension):
+def rename_files(name, extension):
     sequence = 1
     # List Current Files in Directory
     for file in os.listdir():
-
         os.rename(file, "{}{:03d}{}".format(name, sequence, extension[0]))
         sequence += 1
-
-    return print("Files have been renamed!")
+    print("Files have been renamed!")
+    return False
 
 
 def check_files(location):
@@ -26,15 +25,16 @@ def check_files(location):
     for ext in file_list:
         # Checks if
         if ext != file_list[0]:
-            return print("Files in the directory are not all the same file type. Please try another directory.")
+            print("Files in the directory are not all the same file type. Please try another directory.\n"
+                  "*******************************************\n")
+            return True
 
-    return rename_files(location, prefix, extension)
+    return rename_files(prefix, file_list[0])
 
 
-# Set the variables to create filename
-file_directory = input("Directory where files are located?\n")
-prefix = input("Prefix for the file name (same with each filename)?\n")
-
-# Run the rename_files function
-check_files(file_directory)
-
+run = True
+while run:
+    # Set the variables to create filename
+    file_directory = input("Directory where files are located?\n")
+    prefix = input("Prefix for the file name (same with each filename)?\n")
+    run = check_files(file_directory)
