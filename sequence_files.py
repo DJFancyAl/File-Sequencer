64fff4486ea13a567a46c6fdeedca589be1a5273
Al Bluemle <abluemleiv@gmail.com>
2021-03-24 12:58:57 -0400

    Adds the check_files() function to the script
    The check_files function verifies that all files in the working directory are of the same file type. It does this by creating a list of all file extensions in the directory. Then it iterates through the list and compares the extensions of each. If the extensions don't match than the function returns an error message. If they do match, then the rename_files function is called.

--- sequence_files.py
+++ sequence_files.py
@@ -2,20 +2,39 @@ import os
 import re
 
 
-def rename_files(location, prefix):
-    # Change the working directory based off the file_directory input
-    os.chdir(location)
+def rename_files(location, name, extension):
     sequence = 1
     # List Current Files in Directory
     for file in os.listdir():
-        extension = re.findall(r"(\.\w+)$", file)
-        os.rename(file, "{}{:03d}{}".format(prefix, sequence, extension[0]))
+
+        os.rename(file, "{}{:03d}{}".format(name, sequence, extension[0]))
         sequence += 1
+
+    return print("Files have been renamed!")
+
+
+def check_files(location):
+    # Change the working directory based off the file_directory input
+    os.chdir(location)
+
+    file_list = []
+    for file in os.listdir():
+        # Adds all file extensions to a list.
+        extension = re.findall(r"(\.\w+)$", file)
+        file_list.append(extension)
+
+    for ext in file_list:
+        # Checks if
+        if ext != file_list[0]:
+            return print("Files in the directory are not all the same file type. Please try another directory.")
+
+    return rename_files(location, prefix, extension)
 
 
 # Set the variables to create filename
 file_directory = input("Directory where files are located?\n")
 prefix = input("Prefix for the file name (same with each filename)?\n")
 
-rename_files(file_directory, prefix)
-print("Files have been renamed!")
+# Run the rename_files function
+check_files(file_directory)
+
