9481d10c351644d2bee40185dc0bb659d8d97249
Al Bluemle <abluemleiv@gmail.com>
2021-03-26 14:53:28 -0400

    Creates a file_history.csv file with old and new names.
    A lot of changes were made. Old and new name lists are created before checking the file extensions. Both lists are now used to generate the name change demo. Then the lists are pushed to the new create_history function. They're combined into a dictionary and then printed to the csv file in rows.

--- sequence_files.py
+++ sequence_files.py
@@ -1,35 +1,6 @@
 import os
 import re
-
-
-def create_history(name, extension, list):
-    # Display a list of current file names
-    print("New file names will look like this:")
-    i = 0
-    sequence = 1
-    old_name_length = 0
-    for i in range(5):
-        # Define variables
-        old_name = list[i]
-        if len(old_name) > old_name_length:
-            # Sets longest file length for formatting
-            old_name_length = len(old_name)
-        i += 1
-
-    for i in range(5):
-        old_name = list[i]
-        new_name = "{}{:03d}{}".format(name, sequence, extension)
-        print('{:>{}s} ----> {}'.format(old_name, old_name_length, new_name))
-        i += 1
-        sequence += 1
-
-    verify = input("**********************************\nWould you like to continue?\n")
-
-    if verify.lower() == 'y' or verify.lower() == 'yes':
-        pass
-
-    print("Okay. Let's try again.\n")
-    return True
+import csv
 
 
 def rename_files(name, extension):
@@ -42,25 +13,68 @@ def rename_files(name, extension):
     return False
 
 
-def check_files(location):
+def check_files(location, name):
     # Change the working directory based off the file_directory input
     os.chdir(location)
 
     file_list = []
+    new_list = []
+    sequence = 1
+
     for file in os.listdir():
         # Adds all file extensions to a list.
         file_list.append(file)
 
     extension1 = re.findall(r"(\.\w+)$", file_list[0])
     for file in file_list:
-        extension2 = re.findall(r"(\.\w+)$", file)
+        # Create a new_file_list for use in later functions.
+        new_list.append("{}{:03d}{}".format(name, sequence, extension1[0]))
+        sequence += 1
         # Checks if the file extensions are all the same.
+        extension2 = re.findall(r"(\.\w+)$", file)
         if extension1 != extension2:
             print("Files in the directory are not all the same file type. Please try another directory.\n"
                   "*******************************************\n")
             return True
 
-    return create_history(prefix, extension1[0], file_list)
+    return show_demo(file_list, new_list)
+
+
+def show_demo(name_list, new_name_list):
+    # Display a list of current file names
+    print("New file names will look like this:")
+    sequence = 1
+    old_name_length = 0
+    for i in range(5):
+        # Define variables
+        old_name = name_list[i]
+        if len(old_name) > old_name_length:
+            # Sets longest file length for formatting
+            old_name_length = len(old_name)
+        i += 1
+
+    for i in range(5):
+        old_name = name_list[i]
+        new_name = new_name_list[i]
+        print('{:>{}s} ----> {}'.format(old_name, old_name_length, new_name))
+        i += 1
+        sequence += 1
+
+    verify = input("**********************************\nWould you like to continue with file change?\n")
+
+    if verify.lower() == 'y' or verify.lower() == 'yes':
+        return create_history(name_list, new_name_list)
+
+    print("Okay. Let's try again.\n")
+    return True
+
+
+def create_history(old_list, new_list):
+    history_list = dict(zip(old_list, new_list))
+    with open('.file_history.csv', 'w', newline="") as file_csv:
+        writer = csv.writer(file_csv)
+        for file in history_list:
+            writer.writerow([file] + [history_list[file]])
 
 
 run = True
@@ -68,4 +82,4 @@ while run:
     # Set the variables to create filename
     file_directory = input("Directory where files are located?\n")
     prefix = input("Prefix for the file name (same with each filename)?\n")
-    run = check_files(file_directory)
+    run = check_files(file_directory, prefix)
